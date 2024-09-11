bcd5e2e2cb3e1bb19178e34832bad9a200740896
sarahnorton <sarah.norton04@gmail.com>
2024-09-11 19:13:27 +1000

    Half integrated with changes made to main

--- src/main/java/com/example/samplesalad/libraryController.java
+++ src/main/java/com/example/samplesalad/libraryController.java
@@ -2,19 +2,14 @@ package com.example.samplesalad;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.SplitPane;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Popup;
 
 import java.io.IOException;
 
 import static javafx.application.Application.launch;
 
-public class libraryCondensedController {
+public class libraryController extends HelloController {
+
     @FXML // Search bar
     public void onTextEntered(ActionEvent actionEvent) {
         //search through table
@@ -22,6 +17,11 @@ public class libraryCondensedController {
 
     @FXML // side arrow bar
     public void onSideBarClicked(MouseEvent mouseEvent) throws IOException {
-        FXMLLoader expandedLoader = new FXMLLoader(libraryCondensedApplication.class.getResource("library-expanded.fxml"));
+        loadPage("library-expanded.fxml");
+    }
+
+    @FXML
+    private void openCondensedLibrary(MouseEvent event) {
+        loadPage("library-condensed.fxml");
     }
 }
