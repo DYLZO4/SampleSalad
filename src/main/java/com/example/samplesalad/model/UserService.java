ae699e2c977dc8dc20a8fecb7a2bd25bb4c01090
Dylan Hessing <n11245310@qut.edu.au>
2024-09-14 13:01:02 +1000

    moved password hashing into seperate utility class

--- src/main/java/com/example/samplesalad/model/UserService.java
+++ src/main/java/com/example/samplesalad/model/UserService.java
@@ -11,30 +11,18 @@ public class UserService {
     }
 
     public void registerUser(String firstName, String lastName, String password, String email, String phone){
-        User user = new User(firstName, lastName, hashPassword(password), email, phone);
+        User user = new User(firstName, lastName, password, email, phone);
         userDAO.add(user);
     }
 
     public boolean loginUser(String email, String password) {
         User user = userDAO.getByEmail(email);
         if (user != null) {
-            String hashedPassword = hashPassword(password);
+            String hashedPassword = HashUtil.hashPassword(password);
             return hashedPassword.equals(user.getHashedPassword());
         }
         return false;
     }
 
-    private String hashPassword(String password){
-        try{
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hashedBytes = md.digest(password.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for (byte b: hashedBytes){
-                sb.append(String.format("%02x", b));
-            }
-            return sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
+
 }
