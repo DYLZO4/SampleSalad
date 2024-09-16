8cabe5d0928bf34437721d6149369e70ca531b17
Theo Dela Cruz <delacruztheo61@gmail.com>
2024-09-16 16:07:10 +1000

    Added comments to resolve Javadoc generation error

--- src/main/java/com/example/samplesalad/model/Pad.java
+++ src/main/java/com/example/samplesalad/model/Pad.java
@@ -51,7 +51,10 @@ public class Pad {
         System.out.println("pad" + padId + " is unmuted");
     };
 
-
+    /**
+     * Sets the volume of the pad's sample
+     * @param volume the volume of the sample
+     */
     public void setVolume(double volume) {
         this.volume = volume;
         if (sample != null) {
@@ -60,12 +63,19 @@ public class Pad {
         }
     };
 
-
+    /**
+     * Retrieves the keybind
+     * @return keybind the key binding currently mapped to the pad
+     */
     public String getKeybind() {
         System.out.println("keybind is " + keybind);
         return keybind;
     }
 
+    /**
+     * Sets the keybind
+     * @param newKeybind the new key binding to map to the pad
+     */
     public void setKeybind(String newKeybind) {
         System.out.println("keybind has been set to " + newKeybind);
         this.keybind = newKeybind;
@@ -80,11 +90,19 @@ public class Pad {
         return false;
     };
 
-
+    /**
+     * Gets the pad ID.
+     * @return The ID of the pad.
+     */
     public int getPadId() {
         System.out.println("Current pad: pad" +  padId);
         return padId;
     }
+
+    /**
+     * Sets the pad ID.
+     * @param padId The ID to be assigned to this pad.
+     */
     public void setPadId(int padId) {
         System.out.println("Pad has been set as pad" +  padId);
         this.padId = padId;
