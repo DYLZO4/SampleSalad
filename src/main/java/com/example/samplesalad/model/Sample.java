a3f0fc7873342fe953870bafce11ffe06086305d
Dylan Hessing <n11245310@qut.edu.au>
2024-10-20 16:14:34 +1000

    added javadoc comments

--- src/main/java/com/example/samplesalad/model/Sample.java
+++ src/main/java/com/example/samplesalad/model/Sample.java
@@ -295,7 +295,6 @@ public class Sample {
 
     /**
      * Sets the end start in milliseconds
-     * @return endTime The timestamp marking the end of an audio clip.
      */
     public void setStartTime(int msStartTime) {
         startTime = msStartTime;
@@ -303,7 +302,6 @@ public class Sample {
 
     /**
      * Sets the end time in milliseconds
-     * @return endTime The timestamp marking the end of an audio clip.
      */
     public void setEndTime(int msEndTime) {
         endTime = msEndTime;
